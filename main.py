6bf5b8f1d967f3e2b83e072bacf81e75ebb7fc8b
Khair Al Anam <84736189+khairalanam@users.noreply.github.com>
2021-12-09 15:09:40 +0530

    Added Function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import requests
 import json
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+# Fetching data from API
 
 response = requests.get("https://api.exchangerate-api.com/v4/latest/USD")
-text = json.dumps(response.json(), sort_keys=True, indent=4)
-print(text)
+text = response.json()
+currencies = text["rates"]
+
+# Functions used
+
+
+def convert(from_currency, to_currency, amount):
+    usd_amount = amount / currencies[from_currency]
+    pre_final_amount = usd_amount * currencies[to_currency]
+    final_amount = round(pre_final_amount, 4)
+    return final_amount
